4545d6b2a2ac178b73a9cc152b83aeb1b9876617
TanDat-ttd <22130037@st.hcmuaf.edu.vn>
2025-01-06 15:45:28 +0700

    Your commit message

--- tour_booking/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Trang chủ
-    path('about/', views.about, name='about'),  # Trang about
-]
