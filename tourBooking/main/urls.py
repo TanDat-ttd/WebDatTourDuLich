2c814317ec0613258906cd0474f1f148cb019347
Phongntt2904 <Phongntt195@gmail.com>
2025-01-09 01:56:38 +0700

    Chỉnh sửa các đường dẫn tĩnh trong các tệp HTML và cập nhật cấu hình

--- tourBooking/main/urls.py
+++ tourBooking/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views  # Import views của app
 from django.contrib import admin
 urlpatterns = [
-    path('admin/', admin.site.urls),  # URL mặc định cho admin
+    #path('admin/', admin.site.urls),  # URL mặc định cho admin
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
