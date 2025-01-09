378da3e3dab2ebd4914185d412707859b1a6d66b
Phucdev2k4 <phucalex347@gmail.com>
2025-01-09 11:37:46 +0700

    Phuc_capnhatlancuoi

--- tourBooking/tour/urls.py
+++ tourBooking/tour/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views  # Import views của app
 
 urlpatterns = [
-    path('tour/', views.tour, name='tour'),  # Trang tour
-]
+    path('tour', views.tour, name='tour'),  # URL trỏ đến view tour
+    path('tour/<int:id>/', views.tour_detail, name='tour_detail'),  # Trang chi tiết tour
+
+
+]   
